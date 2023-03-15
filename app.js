5cd6a2990aefbbdaa9b6894abfc5882f2cf73e2c
Liam DeWitt <mugzy550@gmail.com>
2023-03-14 21:45:05 -0400

    fix format on playerChoice

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-const RPS = ['Rock', 'Paper', 'Scissors']
 const getComputerChoice = () =>{
-    const randomNum = Math.floor(Math.random() * 3)
+    const RPS = ['rock', 'paper', 'scissors'];
+    const randomNum = Math.floor(Math.random() * 3);
     //using *3 because 3 is exclusive since it's a range that starts at 0
-    console.log(RPS[randomNum])
+    return (RPS[randomNum]);
 }
 
 const playerSelection = (choice) => {
-    choice = choice.toLowerCase();
-    //makes player input lowercase
-    if (typeof choice !== 'string' || !['rock', 'paper', 'scissors'].includes(choice)) {
-      console.log("Please enter Rock, Paper, or Scissors!");
-      return false;
-    }
-    return choice;
+  choice = choice.toLowerCase();
+  //makes player input lowercase
+  if (typeof choice !== 'string' || !['rock', 'paper', 'scissors'].includes(choice)) {
+    console.log("Please enter Rock, Paper, or Scissors!");
+    return false;
   }
+  return choice;
+}
+
